11c7e4e3d182fc31e4bdf91c19c70fd17bf33e7e
Daniel Miranda <developermiranda@gmail.com>
2020-06-22 02:02:56 -0300

    Criando Páginas e Links

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const About = () => {
+    const text = 'meu texto about'
+    return (
+        <>
+            <nav>
+                <Link to="/about" activeStyle={{ color: "red" }} activeClassName="active">About</Link> |      
+                <Link to="/contact" activeStyle={{ color: "red" }} activeClassName="active">Contact</Link>      
+            </nav>
+            <h1>About</h1>
+            <h2>{ text }</h2>
+        </>
+    )
+}
+
+export default About
